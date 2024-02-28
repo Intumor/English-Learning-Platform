4e91c3b13bb323bf768b04202b4cf2bed079421f
Alex Band <lingualex12345@gmail.com>
2024-02-28 00:21:54 -0600

    make the page 4 responsive

--- intro-and-first-lesson.js
+++ intro-and-first-lesson.js
@@ -12,21 +12,16 @@ const showMessage = () => {
   popupMessage.classList.add('popup-clicked');
   const userName = textBar.value;
   const messageHeader = document.createElement('h2');
+  messageHeader.classList.add('popup-header');
   messageHeader.innerText = `Welocme aboard, ${userName}!`;
-  messageHeader.style.fontSize = '3rem';
-  messageHeader.style.textAlign = 'center';
-  messageHeader.style.letterSpacing = '0.3rem'
   messageBox.appendChild(messageHeader);
   const messageText = document.createElement('p');
+  messageText.classList.add('popup-text');
   messageText.innerText = 'Thank you for introducing yourself! I hope you have a great time learning English with us. Even though it might seem like a daunting and intimidating task you have nothing to worry about! We will be assisting you every step of the way. We are here to make this journey fun and exciting and show you that ANYONE can speak English. You do not have to think about the process, we will tell you exactly what to do, every day. So sit back, relax and let us take you on this exciting ride! Just please promise you will do your homework :)';
-  messageText.style.fontSize = '2rem';
-  messageText.style.lineHeight = '3.5rem';
-  messageText.style.textAlign = 'center';
   messageBox.appendChild(messageText);
   const swearButton = document.createElement('button');
+  swearButton.classList.add('swear-button');
   swearButton.innerText = 'Pinky swear';
-  swearButton.style.fontSize = '1.6rem';
-  swearButton.style.margin = '2rem 1rem 2rem 1rem';
   swearButton.onclick = hideMessage;
   messageBox.appendChild(swearButton);
 }
