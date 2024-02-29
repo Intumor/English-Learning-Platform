f123c6c7f7cbe5f255abcc40a8fbdb72bfcad5c0
Alex Band <lingualex12345@gmail.com>
2024-02-28 23:53:45 -0600

    make the pop-up responsive

--- intro-and-first-lesson.js
+++ intro-and-first-lesson.js
@@ -10,20 +10,24 @@ const hideMessage = () => {
 
 const showMessage = () => {
   popupMessage.classList.add('popup-clicked');
-  const userName = textBar.value;
+  let userName = textBar.value;
   const messageHeader = document.createElement('h2');
   messageHeader.classList.add('popup-header');
-  messageHeader.innerText = `Welocme aboard, ${userName}!`;
+  if (textBar.value === "") {
+    userName = "Anonymous";
+  }
+  messageHeader.innerText = `Welcome aboard, ${userName}!`;
   messageBox.appendChild(messageHeader);
   const messageText = document.createElement('p');
   messageText.classList.add('popup-text');
-  messageText.innerText = 'Thank you for introducing yourself! I hope you have a great time learning English with us. Even though it might seem like a daunting and intimidating task you have nothing to worry about! We will be assisting you every step of the way. We are here to make this journey fun and exciting and show you that ANYONE can speak English. You do not have to think about the process, we will tell you exactly what to do, every day. So sit back, relax and let us take you on this exciting ride! Just please promise you will do your homework :)';
+  messageText.innerText = 'Thank you for introducing yourself! I hope you have a great time learning English with us. Even though it might seem like a daunting and intimidating task you have nothing to worry about! We will be assisting you every step of the way. We are here to make this journey fun and exciting and show you that ANYONE can speak English. You do not have to think about organizing your learning process, we will tell you exactly what to do. So just sit back, relax and let us take you on this exciting ride! Just promise me you will do your homework :)';
   messageBox.appendChild(messageText);
   const swearButton = document.createElement('button');
   swearButton.classList.add('swear-button');
   swearButton.innerText = 'Pinky swear';
   swearButton.onclick = hideMessage;
   messageBox.appendChild(swearButton);
+
 }
 
 submitButton.addEventListener('click', showMessage)
