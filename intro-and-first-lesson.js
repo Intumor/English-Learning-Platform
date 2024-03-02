eb06b80ec704661d2971045de438b58dbd674add
Alex Band <lingualex12345@gmail.com>
2024-03-01 21:54:24 -0600

    finish 3 sections

--- intro-and-first-lesson.js
+++ intro-and-first-lesson.js
@@ -2,6 +2,15 @@ const submitButton = document.querySelector('.submit-button');
 const textBar = document.querySelector('.text-bar');
 const popupMessage = document.querySelector('.pop-up-message');
 const messageBox = document.querySelector('.message-box');
+let storedUserName = localStorage.getItem('userName');
+
+if (storedUserName !== "") {
+  submitButton.disabled = true;
+  submitButton.classList.add('disabled')
+  userName = storedUserName;
+} else {
+  userName = "";
+}
 
 const hideMessage = () => {
   popupMessage.classList.remove('popup-clicked');
@@ -17,6 +26,7 @@ const showMessage = () => {
   if (textBar.value === "") {
     userName = "Anonymous";
   }
+  /*localStorage.setItem('userName', userName);*/
   messageHeader.innerText = `Welcome aboard, ${userName}!`;
   messageBox.appendChild(messageHeader);
   const messageText = document.createElement('p');
@@ -31,3 +41,68 @@ const showMessage = () => {
 }
 
 submitButton.addEventListener('click', showMessage);
+textBar.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    showMessage;
+  }
+})
+
+/*first answer*/
+const quizTextbar = document.querySelector('#quiz-textbar');
+const checkButton = document.querySelector('#submit');
+const correct = document.querySelector('#correct');
+const incorrect = document.querySelector('#incorrect');
+let inputValue = "";
+
+const removeChecks = () => {
+  if (quizTextbar.value !== inputValue) {
+    correct.classList.remove('correct-shown');
+    incorrect.classList.remove('incorrect-shown');
+  }
+}
+
+const checkAnswer = () => {
+  let answer = quizTextbar.value.toLowerCase();
+  correct.classList.remove('correct-shown');
+  incorrect.classList.remove('incorrect-shown');
+  if (answer === 'i will live' || answer === 'i will live.' || answer === "i'll live" || answer === "i'll live") {
+    correct.classList.add('correct-shown');
+  } else {
+    incorrect.classList.add('incorrect-shown');
+  }
+
+  inputValue = answer;
+}
+
+checkButton.addEventListener('click', checkAnswer);
+quizTextbar.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    checkAnswer();
+  }
+});
+
+quizTextbar.addEventListener('input', removeChecks);
+
+/*second answer*/
+const quizTextbar2 = document.querySelector('#quiz-textbar-2');
+const checkButton2 = document.querySelector('#submit-2');
+const correct2 = document.querySelector('#correct-2');
+const incorrect2 = document.querySelector('#incorrect-2');
+
+const checkAnswer2 = () => {
+  let answer = quizTextbar2.value.toLowerCase();
+  correct2.classList.remove('correct-shown');
+  incorrect2.classList.remove('incorrect-shown');
+  if (answer === 'he will live' || answer === 'he will live.' || answer === "he'll live" || answer === "he'll live") {
+    correct2.classList.add('correct-shown');
+  } else {
+    incorrect2.classList.add('incorrect-shown');
+  }
+}
+
+checkButton2.addEventListener('click', checkAnswer2);
+quizTextbar2.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    checkAnswer2();
+  }
+})
