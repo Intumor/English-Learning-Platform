d7581951806266c7bffd23f253710188d9dc5ea9
Alex Band <lingualex12345@gmail.com>
2024-03-19 16:38:15 -0600

    finish section-two of lessons6 page

--- lesson6.js
+++ lesson6.js
@@ -96,3 +96,255 @@ const startTimer = () => {
 }
 
 startButton.addEventListener('click', startRecording);
+
+
+const questions = [
+  {id: 1, answer: 'she wants him to sing', done: false, textContent: 'Como se dice "Ella quiere que él cante"?', divider: 'remove'},
+  {id: 2, answer: 'they need her to leave', done: false, textContent: 'Como se dice "Ellos necesitan que ella se vaya"?'},
+  {id: 3, answer: 'i want her to jump', done: false, textContent: 'Como se dice "Quiero que ella salte"?'},
+  {id: 4, answer: 'he asked me to help', done: false, textContent: 'Como se dice "Él me pidió que ayudara"?'},
+  {id: 5, answer: 'we want them to come', done: false, textContent: 'Como se dice "Queremos que ellos vengan"?'},
+  {id: 6, answer: 'they want me to go', done: false, textContent: 'Como se dice "Quieren que yo vaya"?'},
+  {id: 7, answer: 'she needs them to talk', done: false, textContent: 'Como se dice "Ella necesita que ellos hablen"?'},
+  {id: 8, answer: 'sarah asked us to join', done: false, textContent: 'Como se dice "Sarah nos pidió que nos unamos"?'},
+  {id: 9, answer: 'we need them to stop', done: false, textContent: 'Como se dice "Necesitamos que ellos se parar"?'},
+  {id: 10, answer: 'you want me to help', done: false, textContent: 'Como se dice "Quieres que te ayude"?'}
+]
+
+
+const createQuiz = (questionsArr, num) => {
+  const quizHeader = document.querySelector('.quiz-header');
+  const section = document.querySelector(`#section-${num}`);
+  const quiz = document.createElement('div');
+  quiz.className = 'quiz';
+
+
+  questionsArr.forEach((question, index) => {
+    if (question.type === 'new-word') {
+      const newWords = document.createElement('p');
+      newWords.className = 'new-words';
+      newWords.textContent = question.content;
+      section.appendChild(newWords);
+      return;
+    }
+
+    const divider = document.createElement('div');
+    divider.className = 'divider';
+      
+    const questionText = document.createElement('div');
+    questionText.className = 'question';
+
+    const label = document.createElement('label');
+    label.setAttribute('for', `quiz-textbar-${question.id}`);
+    label.textContent = question.textContent;
+
+    const answerBox = document.createElement('div');
+    answerBox.className = 'answer-box';
+
+    const inputBox = document.createElement('div');
+    inputBox.className = 'input-box';
+
+    const quizTextBar = document.createElement('input');
+    quizTextBar.className = 'quiz-textbar';
+    quizTextBar.id = `quiz-textbar-${question.id}`;
+    quizTextBar.type = 'text';
+    quizTextBar.placeholder = 'Your answer...';
+    quizTextBar.autocomplete = 'off';
+
+    const checkBox = document.createElement('div');
+    checkBox.className = 'check-box';
+
+    const submitButton = document.createElement('input');
+    submitButton.className = 'submit';
+    submitButton.id = `submit-${question.id}`;
+    submitButton.type = 'submit';
+    submitButton.value = 'Check';
+
+    const correct = document.createElement('div');
+    correct.className = 'correct';
+    correct.id = `correct-${question.id}`;
+
+    const correctIcon = document.createElement('img');
+    correctIcon.src = "icons/icons8-correct.svg"
+    correctIcon.alt = "correct";
+
+    const incorrect = document.createElement('div');
+    incorrect.className = 'incorrect';
+    incorrect.id = `incorrect-${question.id}`;
+
+    const tryAgainText = document.createElement('p');
+    tryAgainText.textContent = "Try again!";
+
+
+
+    const incorrectIcon = document.createElement('img');
+    incorrectIcon.src = "icons/icons8-wrong.svg"
+    incorrectIcon.alt = "wrong";
+
+
+    const TryAgainText = document.createElement('div');
+    TryAgainText.textContent = "Try again!";
+
+    let inputValue = "";
+
+    section.appendChild(quiz);
+    quiz.appendChild(divider)
+    quiz.appendChild(questionText);
+    questionText.appendChild(label);
+    questionText.appendChild(answerBox);
+    answerBox.appendChild(inputBox);
+    answerBox.appendChild(checkBox);
+    inputBox.appendChild(quizTextBar);
+    checkBox.appendChild(submitButton);
+    checkBox.appendChild(correct);
+    checkBox.appendChild(incorrect);
+    correct.appendChild(correctIcon);
+    incorrect.appendChild(incorrectIcon);
+    incorrect.appendChild(tryAgainText);
+
+    if (index === 0 || question.divider === 'remove') {
+      quiz.removeChild(divider)
+    }
+
+
+    const checkAnswer = () => {
+      let answer = quizTextBar.value.toLowerCase().trim().replace(/[.,]/g, '');
+      answer = answer.replace(/\s\s+/g, " ");
+      const wordsArr = []
+      let word = ""
+      for (let i = 0; i < answer.length; i++) {
+        if (answer[i] !== " ") {
+          word += answer[i];
+        } else {
+          wordsArr.push(word)
+          word = "";
+        }
+      }
+
+      wordsArr.push(word)
+      const corrected = wordsArr.map((word) => {
+        if (word === "didn't") {
+          word = "did not";
+          return word;
+        } else if (word === "don't") {
+          word = "do not";
+          return word;
+        } else if (word === "doesn't") {
+          word = "does not";
+          return word;
+        } else if (word === "i'll") {
+          word = "i will";
+          return word;
+        } else if (word === "you'll") {
+          word = "you will";
+          return word;
+        } else if (word === "we'll") {
+          word = "we will";
+          return word;
+        } else if (word === "they'll") {
+          word = "they will";
+          return word;
+        } else if (word === "he'll") {
+          word = "he will";
+          return word;
+        } else if (word === "she'll") {
+          word = "she will";
+          return word;
+        } else if (word === "it'll") {
+          word = "it will";
+          return word;
+        } else if (word === "i'm") {
+          word = "i am";
+          return word;
+        } else if (word === "you're") {
+          word = "you are";
+          return word;
+        } else if (word === "we're") {
+          word = "we are";
+          return word;
+        } else if (word === "they're") {
+          word = "they are";
+          return word;
+        } else if (word === "he's") {
+          word = "he is";
+          return word;
+        } else if (word === "she's") {
+          word = "she is";
+          return word;
+        } else if (word === "isn't") {
+          word = "is not";
+          return word;
+        } else if (word === "aren't") {
+          word = "are not";
+          return word;
+        } else if (word === "wasn't") {
+          word = "was not";
+          return word;
+        } else if (word === "weren't") {
+          word = "were not";
+          return word;
+        } else {
+          return word;
+        }
+      })
+      answer = corrected;
+      answer = answer.join(' ');
+      correct.classList.remove('correct-shown');
+      incorrect.classList.remove('incorrect-shown');
+
+      if (answer === question.answer) {
+        correct.classList.add('correct-shown');
+      } else if (answer === question.answer2) {
+        correct.classList.add('correct-shown');
+      } else {
+        incorrect.classList.add('incorrect-shown');
+      }
+  
+      question.done = true;
+      inputValue = answer;
+    }
+
+    submitButton.addEventListener('click', checkAnswer);
+    quizTextBar.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter') {
+        checkAnswer();
+      }
+    });
+
+    quizTextBar.addEventListener('input', () => {
+      if (quizTextBar.value !== inputValue) {
+        correct.classList.remove('correct-shown');
+        incorrect.classList.remove('incorrect-shown');
+      }
+    });
+
+    const handleKeyDown = (event) => {
+      if (event.key === 'ArrowDown') {
+        event.preventDefault();
+        const currentIndex = question.id;
+        const nextIndex = currentIndex + 1;
+        const nextTextBar = document.querySelector(`#quiz-textbar-${nextIndex}`);
+        if (nextTextBar === null) {
+          return;
+        }
+        nextTextBar.focus();
+      }
+
+      if (event.key === 'ArrowUp') {
+        event.preventDefault();
+        const currentIndex = question.id;
+        const previousIndex = currentIndex - 1;
+        const previousTextBar = document.querySelector(`#quiz-textbar-${previousIndex}`);
+        if (previousTextBar === null) {
+          return;
+        }
+        previousTextBar.focus()
+      }
+    }
+    
+    quizTextBar.addEventListener('keydown', handleKeyDown);
+
+  });
+}
+
+createQuiz(questions, "two")
